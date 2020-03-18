15c8f4d677f65494499cced6642ecfa71e729660
xxthepersonx <zackstephens54@gmail.com>
2020-03-17 22:50:26 -0400

    Repo Cleanup

--- import.sh
+++ import.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-./dpkg-scanpackages Files /dev/null > Packages
+dpkg-scanpackages -m ./Files /dev/null > Packages
 
 gzip -cf Packages > Packages.gz
 xz -9fkev Packages > Packages.xz
 bzip2 -cf Packages > Packages.bz2
+
+git add -A
+git commit -m "Repo Cleanup"
+git push -u origin master
