bff5ae30e44b2e9d54a4841c8b63830ac408f26d
xxthepersonx <zackstephens54@gmail.com>
2020-03-20 03:14:26 -0400

    Temp remove ZebraAutoFinish

--- import.sh
+++ import.sh
@@ -7,5 +7,5 @@ xz -9fkev Packages > Packages.xz
 bzip2 -cf Packages > Packages.bz2
 
 git add -A
-git commit -m "Added ZebraAutoFinish"
+git commit -m "Temp remove ZebraAutoFinish"
 git push -u origin master
